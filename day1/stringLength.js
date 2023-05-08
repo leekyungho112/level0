5b7392908e450dc6d5a625264f9e8a8e598122fd
kyungho <epsoe@naver.com>
2023-05-08 22:44:53 +0900

    문잘리스트를 문자열로 변환

---
+++ day1/stringLength.js
@@ -0,0 +1,6 @@
+function solution(my_string, n) {
+  const subStr = my_string.slice(0, n);
+  return subStr;
+}
+
+console.log(solution('ProgrammerS123', 11));
---
+++ day2/stringToArray.js
@@ -0,0 +1,6 @@
+function solution(arr) {
+  const strJoin = arr.join('');
+  return strJoin;
+}
+
+console.log(solution(['a', 'b', 'c']));
